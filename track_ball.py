cb64aa62db6647d8acfefb1b054351db39095be1
icyqwq <icyqwq@gmail.com>
2020-02-16 00:51:04 +0800

    Update track_ball.py

--- track_ball.py
+++ track_ball.py
@@ -7,6 +7,8 @@ from machine import UART
 from Maix import GPIO
 from fpioa_manager import *
 
+Px = -0.4
+
 fm.register(34,fm.fpioa.UART1_TX)
 fm.register(35,fm.fpioa.UART1_RX)
 uart_out = UART(UART.UART1, 115200, 8, None, 1, timeout=1000, read_buf_len=4096)
@@ -16,13 +18,11 @@ sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.run(1)
 
-while False:
-    uart_out.write('TEST\n')
-    utime.sleep_ms(100)
-
-target_lab_threshold = (45,   70,  -60,   -30,   0,   40)
+#target_lab_threshold = (12, 71, 30, 70, 2, 50)   #R
+target_lab_threshold = (35, 80, -45, -15, -3, 40)   #G
+prev_area = 0
 while True:
-    img=sensor.snapshot()
+    img = sensor.snapshot()
 
     blobs = img.find_blobs([target_lab_threshold], x_stride = 2, y_stride = 2, pixels_threshold = 100, merge = True, margin = 20)
     if blobs:
@@ -32,21 +32,30 @@ while True:
             if b.area() > max_area:
                 max_area = b.area()
                 target = b
+
+        xerr = 160 - target[5]
+        ux = int(Px * xerr)
+
+        if ux > 100:
+            ux = 100
+        elif ux < -100:
+            ux = -100
+
+        if abs(xerr) < 20:
+            ux = 0
+        #print(ux)
+
         if uart_out.read(4096):
             area = target.area()
-            dx = 120 - target[6]
-            hexlist = [(dx >> 8) & 0xFF, dx & 0xFF, (area >> 16) & 0xFF, (area >> 8) & 0xFF, area & 0xFF]
-            uart_out.write(bytes(hexlist))
-        else:
-            pass
-        print(target.area())
+            hexlist = [ux & 0xFF, (area >> 16) & 0xFF, (area >> 8) & 0xFF, area & 0xFF]
+            a = uart_out.write(bytes(hexlist))
+
+        #print(abs(prev_area - target.area()))
+        prev_area = target.area()
         tmp=img.draw_rectangle(target[0:4])
         tmp=img.draw_cross(target[5], target[6])
-        c=img.get_pixel(target[5], target[6])
-
+        # c=img.get_pixel(target[5], target[6])
     else:
-        if uart_out.read(4096):
-            hexlist = [0x80, 0x00, 0x00, 0x00, 0x00]
-            uart_out.write(bytes(hexlist))
-        else:
-            pass
+        if uart_out.read(1):
+            hexlist = [0x00, 0x00, 0x00, 0x00]
+            a = uart_out.write(bytes(hexlist))
